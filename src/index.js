d5b95f98cce64b299b6868653c9f6001f2affc3c
faramo_zayw <40602472+faramozzayw@users.noreply.github.com>
2020-05-18 16:39:08 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import "./styles.css";
+
+const $ = selector => {
+  let elements = document.querySelectorAll(selector);
+  return Array.prototype.slice.call(elements);
+};
+
+const toDeg = num => `${num}deg`;
+
+const cube = $(".cube")[0];
+
+const axis_x = $("#axis_x")[0];
+const axis_y = $("#axis_y")[0];
+const axis_z = $("#axis_z")[0];
+const axis_arr = [axis_x, axis_y, axis_z];
+
+const update_axis = target => e => {
+  const value = e?.target?.value;
+  cube.style.setProperty(`--${target}`, toDeg(value));
+
+  //console.log(cube.style.getPropertyValue("--x"));
+};
+
+axis_arr.forEach(input =>
+  input.addEventListener("input", update_axis(input.name))
+);
